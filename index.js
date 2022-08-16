04ed2dd19140cec962bafef4f14b69289e1d5556
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-08-16 21:31:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let multiply = 62;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+let random;
+random = Math.ceil(Math.random());
+
+let mod;
+let num3 = 19;
+let num4 = 5;
+mod = num3 %= num4;
+
+let max = Math.max(3, 7, 11, 20);
+
+
